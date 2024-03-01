7e5bed460e51c3e6ddd6d617f3473f01479ae8de
Rocky <rocky.huang.dev@gmail.com>
2024-03-02 01:04:59 +0800

    feat: read and write message to firestore

--- app/src/main/java/com/rocky/whisper/chat/ChatViewModel.kt
+++ app/src/main/java/com/rocky/whisper/chat/ChatViewModel.kt
@@ -2,14 +2,49 @@ package com.rocky.whisper.chat
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.rocky.whisper.WhisperDestinationsArgs.ROOM_ID_ARG
+import com.rocky.whisper.data.Message
+import com.rocky.whisper.data.repository.MessageRepository
+import com.rocky.whisper.di.IoDispatcher
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
+
+data class ChatUiState(
+    val messageList: List<Message> = listOf()
+)
 
 @HiltViewModel
 class ChatViewModel @Inject constructor(
-    savedStateHandle: SavedStateHandle
+    savedStateHandle: SavedStateHandle,
+    private val messageRepository: MessageRepository,
+    @IoDispatcher private val dispatcher: CoroutineDispatcher
 ) : ViewModel() {
 
     val roomId: String = checkNotNull(savedStateHandle[ROOM_ID_ARG])
+
+    private val _uiState = MutableStateFlow(ChatUiState())
+    val uiState: StateFlow<ChatUiState> = _uiState
+
+    fun sendMessage(message: String) {
+        viewModelScope.launch(dispatcher) {
+            messageRepository.sendMessage(roomId, message)
+        }
+    }
+
+    fun fetchMessage() {
+        viewModelScope.launch(dispatcher) {
+            messageRepository.fetchMessage(roomId).collectLatest {
+                _uiState.update { currentState ->
+                    currentState.copy(messageList = it.sortedByDescending { it.timestamp })
+                }
+            }
+        }
+    }
 }
--- app/src/main/java/com/rocky/whisper/data/Message.kt
+++ app/src/main/java/com/rocky/whisper/data/Message.kt
@@ -1,10 +1,16 @@
 package com.rocky.whisper.data
 
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class Message(
-    val senderId: String,
-    val message: String,
-    val timestamp: Long
-)
+    val senderId: String = "",
+    val message: String = "",
+    val timestamp: Long = 0L,
+) {
+    fun isCurrentUser(): Boolean {
+        return senderId == Firebase.auth.uid
+    }
+}
